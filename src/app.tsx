fead97926eedf1f9a16fc0406aa4126cdcfa9cc7
gaohan1994 <871418277@qq.com>
2020-02-20 14:37:52 +0800

    回退版本

--- src/app.tsx
+++ src/app.tsx
@@ -20,6 +20,7 @@ class App extends Component {
 
   /**
    * 指定config的类型声明为: Taro.Config
+   * 回退版本
    *
    * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
    * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
