d35662130b5bbe79a8407f006b153a792cca2a8c
lian0121 <759792991@qq.com>
2020-07-08 17:58:19 +0800

    修改bug、样式

--- src/common/util/config.ts
+++ src/common/util/config.ts
@@ -1,7 +1,7 @@
 
-const merchant = localStorage && localStorage.getItem('merchantId') && localStorage.getItem('merchantId') !== 'undefined' ? localStorage.getItem('merchantId') : '1';
-const MCHIDFist = localStorage && localStorage.getItem('MCHIDFist') && localStorage.getItem('MCHIDFist') !== 'undefined' ? localStorage.getItem('MCHIDFist') : '1';
+const merchant = localStorage && localStorage.getItem('merchantId') && localStorage.getItem('merchantId') !== 'undefined' ? localStorage.getItem('merchantId') : '28';
+const MCHIDFist = localStorage && localStorage.getItem('MCHIDFist') && localStorage.getItem('MCHIDFist') !== 'undefined' ? localStorage.getItem('MCHIDFist') : '28';
 export const BASE_PARAM = {
-    MCHID: merchant && process.env.TARO_ENV === 'h5' ? parseInt(merchant) : 1,
-    MCHIDFist: MCHIDFist && process.env.TARO_ENV === 'h5' ? parseInt(MCHIDFist) : 1,
+    MCHID: merchant && process.env.TARO_ENV === 'h5' ? parseInt(merchant) : 28,
+    MCHIDFist: MCHIDFist && process.env.TARO_ENV === 'h5' ? parseInt(MCHIDFist) : 28,
 };
