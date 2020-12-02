8bdd78f1d93882ee57aa19e79612f91f232b3eb4
lian0121 <759792991@qq.com>
2020-12-02 14:56:45 +0800

    修改地址

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -36,6 +36,8 @@ class GetUserinfo extends Taro.Component<Props, State> {
         const {detail} = params;
         if (detail.errMsg === "getPhoneNumber:ok") {
             const codeRes = await WeixinSDK.getWeixinCode();
+            console.log(codeRes, 'w32323232');
+            
             invariant(codeRes.success, codeRes.msg || '请先登录微信');
             const payload = {
                 encryptedData: detail.encryptedData,
